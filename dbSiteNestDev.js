7d9e9157de9bd629fb6600e143132ad94539173a
Pedro Nest <pedronestjp@gmail.com>
2023-01-27 22:37:19 -0300

    set cors

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -1,30 +1,26 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
+
+const express = require('express')
 const { PrismaClient } = require('@prisma/client')
+
+const app = express()
 const prisma = new PrismaClient()
-const userRoutes = require('./src/routes/userRoutes')
-const userPjRoutes = require('./src/routes/userPjRoutes')
-const productsRoutes = require('./src/routes/productsRoutes')
 const cors = require('cors')
+
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-
 app.use(express.json())
 app.use(cors())
+    
+const userRoutes = require('./src/routes/userRoutes')
+const userPjRoutes = require('./src/routes/userPjRoutes')
+const productsRoutes = require('./src/routes/productsRoutes')
 
 app.use(userRoutes)
 app.use(userPjRoutes)
 app.use(productsRoutes)
 
-app.get ('/home', (req, res)=> {
-    res.send("Hi Pedro")
-})
 app.get ('/teste', (req, res)=> {
     res.send("Hi Pangola")
 })
