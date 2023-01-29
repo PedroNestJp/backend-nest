79ff1c03e27802029a7178598cb579577cf9eebe
Pedro Nest <pedronestjp@gmail.com>
2023-01-29 10:05:48 -0300

    up

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -5,6 +5,13 @@ const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
+const xhr = new XMLHttpRequest();
+xhr.open("POST", "https://bar.other/doc");
+xhr.setRequestHeader("X-PINGOTHER", "pingpong");
+xhr.setRequestHeader("Content-Type", "text/xml");
+xhr.onreadystatechange = handler;
+xhr.send("<person><name>Arun</name></person>");
+
 app.use(express.json())
 app.use(cors())
 
