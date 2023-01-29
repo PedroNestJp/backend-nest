ec44443c10ba8aef8513b3a40e0704c8f01ae368
Pedro Nest <pedronestjp@gmail.com>
2023-01-29 05:48:39 -0300

    up cors option

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -5,12 +5,20 @@ const { PrismaClient } = require('@prisma/client')
 
 const app = express()
 const prisma = new PrismaClient()
-const cors = require('cors')
+const cors=require("cors");
+
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
 app.use(express.json())
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 const userRoutes = require('./src/routes/userRoutes')
 const userPjRoutes = require('./src/routes/userPjRoutes')
@@ -24,7 +32,6 @@ app.get ('/teste', (req, res)=> {
     res.send("Hi Pangola")
 })
 
-app.use(cors())
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
 })
