8f54dbdb3254a711a67c7c790aba138605000eb7
Pedro Nest <pedronestjp@gmail.com>
2023-01-30 21:42:38 -0300

    the service is ok locally

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -1,22 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
 const cors = require("cors");
 const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
-const xhr = new XMLHttpRequest();
-xhr.open("POST", "https://bar.other/doc");
-xhr.setRequestHeader("X-PINGOTHER", "pingpong");
-xhr.setRequestHeader("Content-Type", "text/xml");
-xhr.onreadystatechange = handler;
-xhr.send("<person><name>Arun</name></person>");
-
 app.use(express.json())
 app.use(cors())
 
 app.use('/', Router)
-
 
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
