d133a02a123f9dce274df53e0e1e6acb9b7232b1
Pedro Nest <pedronestjp@gmail.com>
2023-01-31 01:52:59 -0300

    alter /routes

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -6,20 +6,20 @@ const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
+app.options('*', cors())
+
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/', Router)
+app.post('/user/create', cors())
+
 // app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
 // app.del('/products/:id', cors(), function (req, res, next) {
 //   res.json({msg: 'This is CORS-enabled for all origins!'})
 // })
  
-
-app.use(cors())
-app.use(express.json())
-
-app.options('*', cors())
-
-app.use('/', Router)
-app.post('/user/create', cors())
-
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
 })
