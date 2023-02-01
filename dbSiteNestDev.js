3d5eabf55c0a38a5df0a39bb3f74de5ed8240815
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 19:31:06 -0300

    change

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -11,10 +11,10 @@ app.use(express.json())
 app.use(cors())
 app.use('/', Router)
 
-// app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
-// app.del('/products/:id', cors(), function (req, res, next) {
-//  res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
+ app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
+ app.del('/products/:id', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+ })
  
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
