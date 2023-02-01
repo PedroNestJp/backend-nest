d5dadb9872112a59b76d34b3717d8440156f02af
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 15:06:55 -0300

    change

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -4,12 +4,13 @@ require('dotenv').config()
 const cors = require("cors");
 const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
-// const APP_URL = process.env.APP_URL
+const APP_URL = process.env.APP_URL
+import "./cors.config"
 
 app.use(cors())
 
 app.use(express.json())
-app.use(Router)
+app.use('/', Router)
 
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,20 +5,17 @@ const userController = require('../controllers/user.controller')
 const userPjController = require('../controllers/userPj')
 const productController = require('../controllers/product.controller')
 
-
 router.get('/products', productController.getProducts)
 router.get('/products/:id', productController.getproduct)
 router.post('/product/create', productController.createProducts)
 router.delete('/product/delete/:id', productController.deleteProduct)
 router.put('/product/update/:id', productController.upProduct)
 
-
 router.get('/userpj/:id', userPjController.getUserPj)
 router.get('/userspj', userPjController.getUsersPj)
 router.post('/userpj/create', userPjController.createUserPjEnd)
 router.delete('/userpj/delete/:id', userPjController.deleteUserPj)
 router.put('/userpj/update/:id', userPjController.upUserPJ)
-
 
 router.get('/user/:id', userController.getUser)
 router.get('/users', userController.getUsers)
