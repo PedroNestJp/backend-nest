46c0888836341d2af59f634c1087bfaaad0204a1
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 02:05:35 -0300

    change

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -7,17 +7,6 @@ const PORT = process.env.PORT
 // const APP_URL = process.env.APP_URL
 
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "*"
-    ); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
 
 app.use(express.json())
 app.use(Router)
