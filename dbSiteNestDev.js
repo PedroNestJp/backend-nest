5b91ea4bd9ec6b1866b8dceb5185ad632ca7acda
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 15:22:53 -0300

    change

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
+const express = require('express')
 const cors = require("cors");
+const app = express()
 const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
-const Headers = require("./cors.config")
 
-app.use(cors(), Headers)
+
 app.use(express.json())
+app.use(cors())
+app.options('*', cors())
 
-app.use(Router)
 
+app.use('/', cors(), Router)
+
+// app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
+// app.del('/products/:id', cors(), function (req, res, next) {
+//  res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+ 
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
 })
