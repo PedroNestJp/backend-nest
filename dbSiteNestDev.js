b40d5ed060d67889c439073f90d1c78543090b5d
Pedro Nest <pedronestjp@gmail.com>
2023-02-01 00:40:19 -0300

    change

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -6,9 +6,20 @@ const Router = require('./src/routes/routes');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
 
-app.use(express.json())
 app.use(cors())
-
+app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "*"
+    ); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  
+app.use(express.json())
 app.use('/', Router)
 
 // app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
