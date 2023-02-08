0b189e7b162655c3fd70f251ae56fce06002f4d4
Pedro Nest <pedronestjp@gmail.com>
2023-02-08 01:52:02 -0300

    change controlled user prisma

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -13,11 +13,10 @@ app.use(express.json())
 app.use(cors())
 app.use('/', Router)
 
-app.options('*', cors()) // enable pre-flight request for DELETE request
-app.put('*', cors())
-app.delete('*', cors(), function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-//  })
+app.options('/user/create', cors()) // enable pre-flight request for DELETE request
+app.delete('/user/create', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
  
 app.listen (process.env.PORT || 8000, ()=>{
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
