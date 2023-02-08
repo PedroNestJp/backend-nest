4053697c9e8abc5fd0828aed45d537973cee00a0
Pedro Nest <pedronestjp@gmail.com>
2023-02-08 01:54:48 -0300

    change controlled user prisma

--- dbSiteNestDev.js
+++ dbSiteNestDev.js
@@ -3,11 +3,26 @@ const express = require('express')
 const cors = require("cors");
 const app = express()
 const Router = require('./src/routes/routes');
-const { headers } = require('./cors.config');
 const PORT = process.env.PORT
 const APP_URL = process.env.APP_URL
-app.use(headers())
 
+module.exports = {
+    async headers() {
+        return [
+          {
+            // matching all API routes
+            source: '/:path*',
+            headers: [
+              { key: 'Access-Control-Allow-Credentials', value: 'true' },
+              { key: 'Access-Control-Allow-Origin', value: '*' },
+              { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+              { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+            ],
+          },
+        ];
+      },
+
+}
 
 app.use(express.json())
 app.use(cors())
