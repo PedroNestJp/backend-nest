51c031695dfaabcc5a00033752d2299202cc1176
Pedro Nest <pedronestjp@gmail.com>
2023-04-17 03:04:01 -0300

    up

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const APP_URL = process.env.APP_URL
-const Routes = require('./src/routes/routes');
 const cors = require('cors');
+const Routes = require('./src/routes/routes');
 
-app.use(
-    cors({
-      origin: "*",
-    })
-  );
+
 app.use(express.json());
 
+app.use(cors());
+
+app.set('Access-Control-Allow-Origin', '*')
 app.use('/', Routes )
+
 
 app.listen (PORT || 8000, () => {
     console.log(`✔ service run on address ${APP_URL} at the port: ${PORT}`)
